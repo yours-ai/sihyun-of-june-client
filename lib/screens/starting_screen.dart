ea675923e8da5b92775abb82809d674032f704b5
Chanuk Kim <cksdnr987@gmail.com>
2023-09-01 16:16:01 +0900

    stateful widget으로 변경, server error snackbar 적용

--- lib/screens/starting_screen.dart
+++ lib/screens/starting_screen.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:go_router/go_router.dart';
-import 'package:project_june_client/contrib/flutter_hooks.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class StartingScreen extends HookWidget {
+class StartingScreen extends StatefulWidget {
   const StartingScreen({super.key});
 
   @override
+  State createState() => _StartingScreen();
+}
+
+class _StartingScreen extends State<StartingScreen> {
+  _checkAuthAndLand() async {
+    final prefs = await SharedPreferences.getInstance();
+    // final isLandingViewed =
+    //     prefs.getBool('isLandingViewed') ?? false; // TODO: 로그인 체크로 변경
+    if (!context.mounted) return;
+    FlutterNativeSplash.remove();
+    context.go('/landing');
+    // if (isLandingViewed) {
+    //   context.go('/login');
+    // } else {
+    //   context.go('/landing');
+    // }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkAuthAndLand();
+  }
+
+  @override
   Widget build(context) {
-    useAsyncEffect(() async {
-      final prefs = await SharedPreferences.getInstance();
-      // final isLandingViewed =
-      //     prefs.getBool('isLandingViewed') ?? false; // TODO: 로그인 체크로 변경
-      if (!context.mounted) return;
-      FlutterNativeSplash.remove();
-      context.go('/landing');
-      // if (isLandingViewed) {
-      //   context.go('/login');
-      // } else {
-      //   context.go('/landing');
-      // }
-      return null;
-    }, []);
-    return Container();
+    return const Scaffold();
   }
 }
