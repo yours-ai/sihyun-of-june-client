660328ffe0e555b8ed47e56c4d163208a22f34b2
boxyhn <parksh937@naver.com>
2023-09-18 15:50:39 +0900

    전화번호 로그인 동의 모달창

--- lib/screens/phone_login_screen.dart
+++ lib/screens/phone_login_screen.dart
@@ -1,14 +1,10 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:project_june_client/actions/auth/dtos.dart';
 import 'package:project_june_client/constants.dart';
 import 'package:project_june_client/widgets/phone_login/name_tab.dart';
 import 'package:project_june_client/widgets/phone_login/verify_tab.dart';
 import 'package:project_june_client/widgets/phone_login/phone_tab.dart';
-import 'package:url_launcher/url_launcher.dart';
 
-import '../widgets/modal_widget.dart';
 
 class PhoneLoginScreen extends StatefulWidget {
   const PhoneLoginScreen({super.key});
@@ -21,74 +17,6 @@ class _PhoneLoginScreen extends State<PhoneLoginScreen> {
   int _tab = 0;
   ValidatedPhoneDTO? validatedPhoneDTO;
   ValidatedAuthCodeDTO? validatedAuthDTO;
-
-  void _showModal() async {
-    await showModalBottomSheet<void>(
-      context: context,
-      useRootNavigator: true,
-      builder: (BuildContext context) {
-        return ModalWidget(
-          title: '시작하려면 동의해주세요',
-          description: Padding(
-            padding: const EdgeInsets.only(top: 26),
-            child: RichText(
-              text: TextSpan(
-                  style: TextStyle(
-                      fontFamily: 'MaruBuri',
-                      fontSize: 15,
-                      color: ColorConstants.black),
-                  children: [
-                    TextSpan(
-                        style: const TextStyle(
-                          decoration: TextDecoration.underline,
-                        ),
-                        text: '이용약관',
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => launchUrl(Uri.parse(Urls.terms))),
-                    const TextSpan(text: ' 및 '),
-                    TextSpan(
-                        style: const TextStyle(
-                          decoration: TextDecoration.underline,
-                        ),
-                        text: '개인정보처리방침',
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => launchUrl(Uri.parse(Urls.privacy))),
-                    const TextSpan(text: '에 동의해야 시작할 수 있어요.'),
-                  ]),
-            ),
-          ),
-          choiceColumn: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              FilledButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(ColorConstants.background),
-                ),
-                onPressed: () {
-                  context.pop();
-                },
-                child: Text(
-                  '취소',
-                  style: TextStyle(
-                      fontSize: 14.0, color: ColorConstants.secondary),
-                ),
-              ),
-              FilledButton(
-                onPressed: () => context.go('/'),
-                child: const Text(
-                  '동의하고 시작하기',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
 
   void handleSmsSend(ValidatedPhoneDTO dto) {
     setState(() {
@@ -104,12 +32,6 @@ class _PhoneLoginScreen extends State<PhoneLoginScreen> {
     });
   }
 
-  void handleSmsLogin(dynamic dto) {
-    setState(() {
-      context.go('/');
-    });
-  }
-
   @override
   Widget build(context) {
     return Scaffold(
@@ -120,14 +42,12 @@ class _PhoneLoginScreen extends State<PhoneLoginScreen> {
               )
             : _tab == 1
                 ? VerifyTabWidget(
-                    dto: this.validatedPhoneDTO!,
+                    dto: validatedPhoneDTO!,
                     onSmsVerify: handleVerify,
-                    onSmsLogin: handleSmsLogin,
                   )
                 : _tab == 2
                     ? NameTabWidget(
-                        dto: this.validatedAuthDTO!,
-                        onSmsLogin: handleSmsLogin,
+                        dto: validatedAuthDTO!,
                       )
                     : Container(),
       ),
--- lib/widgets/phone_login/name_tab.dart
+++ lib/widgets/phone_login/name_tab.dart
@@ -1,16 +1,19 @@
 import 'package:cached_query_flutter/cached_query_flutter.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:project_june_client/actions/auth/dtos.dart';
 import 'package:project_june_client/actions/auth/queries.dart';
 import 'package:project_june_client/constants.dart';
 import 'package:project_june_client/widgets/common/title_layout.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../modal_widget.dart';
 
 class NameTabWidget extends StatefulWidget {
-  final void Function(ValidatedUserDTO dto) onSmsLogin;
   final ValidatedAuthCodeDTO dto;
 
-  const NameTabWidget({Key? key, required this.onSmsLogin, required this.dto})
-      : super(key: key);
+  const NameTabWidget({Key? key, required this.dto}) : super(key: key);
 
   @override
   State<NameTabWidget> createState() => _NameTabWidgetState();
@@ -55,6 +58,88 @@ class _NameTabWidgetState extends State<NameTabWidget> {
     return null;
   }
 
+  void _showSignInModal(ValidatedUserDTO dto) async {
+    await showModalBottomSheet<void>(
+      context: context,
+      useRootNavigator: true,
+      builder: (BuildContext context) {
+        return MutationBuilder(
+          mutation: getSmsTokenMutation(
+            onSuccess: (res, arg) {
+              context.go('/');
+            },
+            onError: (arg, error, fallback) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(error.toString()),
+                ),
+              );
+            },
+          ),
+          builder: (context, state, mutate) => ModalWidget(
+            title: '시작하려면 동의해주세요',
+            description: Padding(
+              padding: const EdgeInsets.only(top: 26),
+              child: RichText(
+                text: TextSpan(
+                    style: TextStyle(
+                        fontFamily: 'MaruBuri',
+                        fontSize: 15,
+                        color: ColorConstants.black),
+                    children: [
+                      TextSpan(
+                          style: const TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                          text: '이용약관',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => launchUrl(Uri.parse(Urls.terms))),
+                      const TextSpan(text: ' 및 '),
+                      TextSpan(
+                          style: const TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                          text: '개인정보처리방침',
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () => launchUrl(Uri.parse(Urls.privacy))),
+                      const TextSpan(text: '에 동의해야 시작할 수 있어요.'),
+                    ]),
+              ),
+            ),
+            choiceColumn: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FilledButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorConstants.background),
+                  ),
+                  onPressed: () {
+                    context.pop();
+                  },
+                  child: Text(
+                    '취소',
+                    style: TextStyle(
+                        fontSize: 14.0, color: ColorConstants.secondary),
+                  ),
+                ),
+                FilledButton(
+                  onPressed: () => mutate(dto),
+                  child: const Text(
+                    '동의하고 시작하기',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   void dispose() {
     firstNameController.dispose();
@@ -64,108 +149,89 @@ class _NameTabWidgetState extends State<NameTabWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final mutation = getSmsTokenMutation(
-      onSuccess: (res, arg) {
-        widget.onSmsLogin(getValidatedData());
-      },
-      onError: (arg, error, fallback) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(error.toString()),
-          ),
-        );
-      },
-    );
-
-    return MutationBuilder(
-      mutation: mutation,
-      builder: (context, state, mutate) => Form(
-        key: _formKey,
-        child: TitleLayout(
-          titleText: '이름을 알려주세요.',
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 28.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IntrinsicWidth(
-                      child: TextFormField(
-                        validator: (value) =>
-                            _validator(value, lastNameController),
-                        controller: lastNameController,
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 0, vertical: 0),
-                          errorStyle: TextStyle(
-                            fontSize: 0,
-                          ),
-                          hintText: '성',
-                          hintStyle: TextStyle(
-                              fontFamily: 'MaruBuri',
-                              fontSize: 25,
-                              color: ColorConstants.neutral),
-                          border: InputBorder.none,
+    return Form(
+      key: _formKey,
+      child: TitleLayout(
+        titleText: '이름을 알려주세요.',
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 28.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IntrinsicWidth(
+                    child: TextFormField(
+                      validator: (value) =>
+                          _validator(value, lastNameController),
+                      controller: lastNameController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 0, vertical: 0),
+                        errorStyle: const TextStyle(
+                          fontSize: 0,
                         ),
-                        style: TextStyle(
+                        hintText: '성',
+                        hintStyle: TextStyle(
                             fontFamily: 'MaruBuri',
                             fontSize: 25,
-                            color: ColorConstants.primary),
+                            color: ColorConstants.neutral),
+                        border: InputBorder.none,
                       ),
+                      style: TextStyle(
+                          fontFamily: 'MaruBuri',
+                          fontSize: 25,
+                          color: ColorConstants.primary),
                     ),
-                    const SizedBox(width: 15),
-                    Expanded(
-                      child: TextFormField(
-                        validator: (value) =>
-                            _validator(value, firstNameController),
-                        controller: firstNameController,
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 0, vertical: 0),
-                          errorStyle: TextStyle(
-                            fontSize: 0,
-                          ),
-                          hintText: '이름',
-                          hintStyle: TextStyle(
-                              fontFamily: 'MaruBuri',
-                              fontSize: 25,
-                              color: ColorConstants.neutral),
-                          border: InputBorder.none,
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: TextFormField(
+                      validator: (value) =>
+                          _validator(value, firstNameController),
+                      controller: firstNameController,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 0, vertical: 0),
+                        errorStyle: const TextStyle(
+                          fontSize: 0,
                         ),
-                        style: TextStyle(
+                        hintText: '이름',
+                        hintStyle: TextStyle(
                             fontFamily: 'MaruBuri',
                             fontSize: 25,
-                            color: ColorConstants.primary),
+                            color: ColorConstants.neutral),
+                        border: InputBorder.none,
                       ),
+                      style: TextStyle(
+                          fontFamily: 'MaruBuri',
+                          fontSize: 25,
+                          color: ColorConstants.primary),
                     ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Text(errorMessage,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        fontFamily: 'MaruBuri',
-                        fontSize: 12,
-                        color: Colors.red)),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(errorMessage,
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      fontFamily: 'MaruBuri', fontSize: 12, color: Colors.red)),
+            ],
           ),
-          actions: OutlinedButton(
-            onPressed: () {
-              if (_formKey.currentState != null &&
-                  _formKey.currentState!.validate()) {
-                mutate(getValidatedData());
-              }
-            },
-            child: const Text('다음'),
-            style:
-                OutlinedButton.styleFrom(padding: const EdgeInsets.all(16.0)),
-          ),
+        ),
+        actions: OutlinedButton(
+          onPressed: () {
+            if (_formKey.currentState != null &&
+                _formKey.currentState!.validate()) {
+              _showSignInModal(getValidatedData());
+            }
+          },
+          style: OutlinedButton.styleFrom(padding: const EdgeInsets.all(16.0)),
+          child: const Text('다음'),
         ),
       ),
     );
