a95ed37fefc17642fdb21b9b9251b60d1ec61ceb
boxyhn <parksh937@naver.com>
2023-09-19 16:51:18 +0900

    mail_detail_screen 에서 읽게 수정

--- lib/widgets/mail_widget.dart
+++ lib/widgets/mail_widget.dart
@@ -14,82 +14,66 @@ class MailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mutation = getReadMailMutation(
-        refetchQueries: ['character-sent-mail-list'],
-        onSuccess: (res, arg) {
-          context.push('/mails/detail/${mail.id}');
-        },
-        onError: (arg, err, fallback) {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text('메일을 읽지 못했습니다. 에러가 계속되면 고객센터에 문의해주세요.'),
-          ));
-        });
     return Center(
-      child: MutationBuilder(
-        mutation: mutation,
-        builder: (context, state, mutate) {
-          return TextButton(
-            onPressed: () {
-              if (mail.is_read) {
-                context.push('/mails/detail/${mail.id}');
-                return;
-              }
-              mutate(mail.id);
-            },
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                const SizedBox(
-                  height: 57,
-                  width: 50,
+      child: TextButton(
+        onPressed: () {
+          if (mail.is_read) {
+            context.push('/mails/detail/${mail.id}');
+            return;
+          }
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const SizedBox(
+              height: 57,
+              width: 50,
+            ),
+            Positioned(
+              top: 0,
+              child: Icon(
+                  mail.is_read == true
+                      ? (PhosphorIcons.envelope_simple_open_thin)
+                      : (PhosphorIcons.envelope_simple),
+                  color: mail.is_read == true
+                      ? ColorConstants.neutral
+                      : ColorConstants.primary,
+                  size: 50),
+            ),
+            Positioned(
+              top: 43,
+              child: Text(
+                mail.available_at.toString().substring(5, 10),
+                style: TextStyle(
+                  fontWeight: mail.is_read == true
+                      ? FontWeight.normal
+                      : FontWeight.bold,
+                  color: mail.is_read == true
+                      ? ColorConstants.neutral
+                      : ColorConstants.primary,
+                  fontSize: 10,
                 ),
-                Positioned(
-                  top: 0,
-                  child: Icon(
-                      mail.is_read == true
-                          ? (PhosphorIcons.envelope_simple_open_thin)
-                          : (PhosphorIcons.envelope_simple),
-                      color: mail.is_read == true
-                          ? ColorConstants.neutral
-                          : ColorConstants.primary,
-                      size: 50),
-                ),
-                Positioned(
-                  top: 43,
-                  child: Text(
-                    mail.available_at.toString().substring(5, 10),
-                    style: TextStyle(
-                      fontWeight: mail.is_read == true
-                          ? FontWeight.normal
-                          : FontWeight.bold,
-                      color: mail.is_read == true
-                          ? ColorConstants.neutral
-                          : ColorConstants.primary,
-                      fontSize: 10,
+              ),
+            ),
+            mail.is_read == true
+                ? const SizedBox(
+                    height: 0,
+                    width: 0,
+                  )
+                : Positioned(
+                    top: 5,
+                    left: 39,
+                    child: Container(
+                      height: 10,
+                      width: 10,
+                      decoration: BoxDecoration(
+                        color: ColorConstants.alert,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
                     ),
                   ),
-                ),
-                mail.is_read == true
-                    ? const SizedBox(
-                        height: 0,
-                        width: 0,
-                      )
-                    : Positioned(
-                        top: 5,
-                        left: 39,
-                        child: Container(
-                          height: 10,
-                          width: 10,
-                          decoration: BoxDecoration(
-                            color: ColorConstants.alert,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                        ),
-                      ),
-              ],
-            ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
