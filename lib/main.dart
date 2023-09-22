3165d14607601c1bd3043b60fb2af03651715a20
Chanuk Kim <cksdnr987@gmail.com>
2023-09-22 18:14:33 +0900

    Amplitude 로그 기록

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:amplitude_flutter/amplitude.dart';
 import 'package:cached_query_flutter/cached_query_flutter.dart';
 import 'package:cached_storage/cached_storage.dart';
+import 'package:amplitude_flutter/identify.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -47,6 +49,10 @@ Future<void> _initialize() async {
     systemNavigationBarIconBrightness: Brightness.dark,
     statusBarColor: Colors.transparent, // status bar color
   ));
+  if (BuildTimeEnvironments.amplitudeApiKey.isNotEmpty) {
+    final Amplitude amplitude = Amplitude.getInstance();
+    amplitude.init(BuildTimeEnvironments.amplitudeApiKey);
+  }
 }
 
 void main() async {
