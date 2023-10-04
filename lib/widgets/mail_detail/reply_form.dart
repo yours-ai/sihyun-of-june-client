051156f850736acb7d08c79f0c7cdcf949c7ba8d
ShihyunPark <parksh937@naver.com>
2023-10-04 22:48:35 +0900

    답장 확인 모달

--- lib/widgets/mail_detail/reply_form.dart
+++ lib/widgets/mail_detail/reply_form.dart
@@ -3,6 +3,7 @@ import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:project_june_client/widgets/mail_detail/mail_info.dart';
+import 'package:project_june_client/widgets/modal_widget.dart';
 
 import '../../actions/mails/dtos.dart';
 import '../../actions/mails/models/Mail.dart';
@@ -48,6 +49,53 @@ class _ReplyFormWidgetState extends State<ReplyFormWidget> {
         );
       },
     );
+    _showConfirmModal() async {
+      await showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return ModalWidget(
+            title: '답장을 모두 작성하셨나요?',
+            description: Padding(
+              padding: const EdgeInsets.only(top:20),
+              child: Text('답장을 보내면 수정이 불가능해요.🥲'),
+            ),
+            choiceColumn: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FilledButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(ColorConstants.background),
+                  ),
+                  onPressed: () {
+                    context.pop();
+                  },
+                  child: Text(
+                    '아니요',
+                    style: TextStyle(
+                        fontSize: 14.0, color: ColorConstants.secondary),
+                  ),
+                ),
+                MutationBuilder(
+                  mutation: mutation,
+                  builder: (context, state, mutate) => FilledButton(
+                    onPressed: () => mutate(getReplyDTO()),
+                    child: const Text(
+                      '네',
+                      style: TextStyle(
+                        fontFamily: 'MaruBuri',
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+
     final mailDueTimeLabel =
         mailService.getMailDueTimeLabel(widget.mail.available_at);
     return Column(
@@ -58,79 +106,76 @@ class _ReplyFormWidgetState extends State<ReplyFormWidget> {
           toFullName: widget.mail.by_full_name,
           availableAt: clock.now(),
         ),
-        MutationBuilder(
-          mutation: mutation,
-          builder: (context, mutationState, mutate) => Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return '답장을 입력해주세요.';
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '답장을 입력해주세요.';
+                  }
+                  return null;
+                },
+                controller: controller,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                minLines: 8,
+                maxLength: 1000,
+                decoration: InputDecoration(
+                  errorText: '',
+                  counterText: controller.text.length > 900
+                      ? '${controller.text.length}/1000'
+                      : '',
+                  errorStyle: TextStyle(
+                    color: ColorConstants.black,
+                  ),
+                  hintText: '답장을 적어주세요...',
+                  hintStyle: TextStyle(
+                      fontFamily: 'MaruBuri',
+                      fontSize: 14,
+                      color: ColorConstants.primary),
+                  border: InputBorder.none,
+                ),
+                style: TextStyle(
+                  fontFamily: 'MaruBuri',
+                  fontSize: 14,
+                  color: ColorConstants.primary,
+                  height: 1.5,
+                ),
+                onChanged: (text) {
+                  setState(() {});
+                },
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0),
+                child: FilledButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _showConfirmModal();
                     }
-                    return null;
                   },
-                  controller: controller,
-                  maxLines: null,
-                  keyboardType: TextInputType.multiline,
-                  minLines: 8,
-                  maxLength: 1000,
-                  decoration: InputDecoration(
-                    errorText: '',
-                    counterText: controller.text.length > 900
-                        ? '${controller.text.length}/1000'
-                        : '',
-                    errorStyle: TextStyle(
-                      color: ColorConstants.black,
-                    ),
-                    hintText: '답장을 적어주세요...',
-                    hintStyle: TextStyle(
-                        fontFamily: 'MaruBuri',
-                        fontSize: 14,
-                        color: ColorConstants.primary),
-                    border: InputBorder.none,
-                  ),
-                  style: TextStyle(
-                    fontFamily: 'MaruBuri',
-                    fontSize: 14,
-                    color: ColorConstants.primary,
-                    height: 1.5,
-                  ),
-                  onChanged: (text) {
-                    setState(() {});
-                  },
-                ),
-                SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: FilledButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        mutate(getReplyDTO());
-                      }
-                    },
-                    child: const Text(
-                      '답장하기',
-                      style: TextStyle(
-                        fontFamily: 'MaruBuri',
-                        fontSize: 14,
-                      ),
+                  child: const Text(
+                    '답장하기',
+                    style: TextStyle(
+                      fontFamily: 'MaruBuri',
+                      fontSize: 14,
                     ),
                   ),
                 ),
-                SizedBox(height: 10),
-                Center(
-                    child: Text(
-                  mailDueTimeLabel,
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: ColorConstants.secondary,
-                  ),
-                )),
-              ],
-            ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                  child: Text(
+                mailDueTimeLabel,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: ColorConstants.secondary,
+                ),
+              )),
+            ],
           ),
         ),
       ],
