06997ea678a836a0e4737522560fde9317d7c62f
ShihyunPark <parksh937@naver.com>
2023-11-13 19:00:47 +0900

    PR 리뷰 수정

--- lib/services/mail_service.dart
+++ lib/services/mail_service.dart
@@ -24,18 +24,18 @@ class MailService {
     return "오늘 저녁 9시";
   }
 
-  int getMailDateDiff(DateTime dt, DateTime firstMailDate) {
-    DateTime normalizedDt = DateTime(dt.year, dt.month, dt.day);
+  int getMailDateDiff(DateTime targetDate, DateTime firstMailDate) {
+    DateTime normalizedDt = DateTime(targetDate.year, targetDate.month, targetDate.day);
     DateTime normalizedFirstMailDate =
         DateTime(firstMailDate.year, firstMailDate.month, firstMailDate.day);
     return normalizedDt.difference(normalizedFirstMailDate).inDays;
   }
 
-  String getMailReceiveDateStr(DateTime dt, bool needMonth) {
-    if (needMonth || dt.day == 1) {
-      return DateFormat('M월 d일').format(dt);
+  String getMailReceiveDateStr(DateTime targetDate, bool needMonth) {
+    if (needMonth || targetDate.day == 1) {
+      return DateFormat('M월 d일').format(targetDate);
     }
-    return DateFormat('d').format(dt);
+    return DateFormat('d').format(targetDate);
   }
 
   String formatMailDate(DateTime dt) {
@@ -49,19 +49,12 @@ class MailService {
   }
 
   Widget calendarWeekday() {
+    final List<String> weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     return Row(
       mainAxisAlignment:
       MainAxisAlignment.spaceAround,
       children: <Widget>[
-        for (var day in [
-          "Sun",
-          "Mon",
-          "Tue",
-          "Wed",
-          "Thu",
-          "Fri",
-          "Sat"
-        ])
+        for (var day in weekdays)
           Text(day,
               style: TextStyle(
                   color: ColorConstants.gray,
@@ -96,8 +89,5 @@ class MailService {
         return '열한 번째 달';
     }
     return '';
-  }
-  String formatMailDate(DateTime dt) {
-    return DateFormat('yyyy.MM.dd').format(dt);
   }
 }
