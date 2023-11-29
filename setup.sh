0f2bdbab2dfc8dae4836a83a41882596e47876be
Minseok Kim <87017787+Arsture@users.noreply.github.com>
2023-11-29 18:27:19 +0900

    feature: firebase client단에서 dev서버 분리 (#90)
    * setup 변경
    
    * 줄 삭제

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@
 # init flutter icons
 if [[ -n "$IS_LIVE" ]]; then
     dart run flutter_launcher_icons -f flutter_launch_icon_live.yaml
+    cp ./firebase/live/google-services.json ./android/app/
+    cp ./firebase/live/GoogleService-Info.plist ./ios/Runner/
+    cp ./firebase/live/firebase_options.dart ./lib/
 else
     dart run flutter_launcher_icons -f flutter_launch_icon_dev.yaml
+    cp ./firebase/dev/google-services.json ./android/app/
+    cp ./firebase/dev/GoogleService-Info.plist ./ios/Runner/
+    cp ./firebase/dev/firebase_options.dart ./lib/
 fi
 
 # init flutter splashs
