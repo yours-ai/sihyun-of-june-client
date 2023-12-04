781bcf51a670d60a28bcd1b6f0b919c792274720
ShihyunPark <parksh937@naver.com>
2023-12-04 16:33:10 +0900

    템플릿 환경변수로 분리

--- lib/services/share_service.dart
+++ lib/services/share_service.dart
@@ -1,10 +1,11 @@
 import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
+import 'package:project_june_client/environments.dart';
 
 class ShareService {
   const ShareService();
 
   void kakaoShare(String? refCode) async {
-    int templateId = 101441;
+    int templateId = BuildTimeEnvironments.kakaoTemplateId;
     Map<String, String> templateArgs = {
       'ref': refCode ?? 'refCode',
     };
