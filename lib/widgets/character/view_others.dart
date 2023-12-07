758b79f6e9b2e08821e3a5d7f421b840e064c48c
Arsture <minseok.kim@pygmalion.team>
2023-12-07 11:54:20 +0900

    불필요 함수 제거

--- lib/widgets/character/view_others.dart
+++ lib/widgets/character/view_others.dart
@@ -13,55 +13,6 @@ import 'package:project_june_client/services.dart';
 import 'package:project_june_client/services/unique_cachekey_service.dart';
 import 'package:project_june_client/widgets/common/dotted_underline.dart';
 
-Widget makeViewOthersWidget(
-    BuildContext context, WidgetRef ref, Character character) {
-  final mainImageSrc =
-      characterService.getMainImage(character.character_info!.images!);
-
-  return GestureDetector(
-    onTap: () {
-      if (!character.is_active) {
-        return;
-      }
-      num id = character.id;
-      context.push('/other-character/$id');
-    },
-    child: ClipRRect(
-      child: Padding(
-        padding: const EdgeInsets.all(7.0),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: ExtendedImage.network(
-                  mainImageSrc,
-                  timeLimit: ref.watch(imageCacheDurationProvider),
-                  cacheKey: UniqueCacheKeyService.makeUniqueKey(mainImageSrc),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            if (!character.is_active) ...[
-              ...characterService.addBlur(),
-              Center(
-                child: Text(
-                  "공개\n예정",
-                  style: TextStyle(
-                    color: ColorConstants.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ],
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
 class ViewOthersWidget extends ConsumerWidget {
   final num excludeId;
 
@@ -94,14 +45,60 @@ class ViewOthersWidget extends ConsumerWidget {
                   .where((character) => character.id != excludeId)
                   .toList();
               return GridView.count(
-                mainAxisSpacing: 10.0,
-                shrinkWrap: true,
-                crossAxisCount: 3,
-                children: filteredCharacters
-                    .map((character) =>
-                        makeViewOthersWidget(context, ref, character))
-                    .toList(),
-              );
+                  mainAxisSpacing: 10.0,
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  children: filteredCharacters.map(
+                    (character) {
+                      final mainImageSrc = characterService
+                          .getMainImage(character.character_info!.images!);
+                      return GestureDetector(
+                        onTap: () {
+                          if (!character.is_active) {
+                            return;
+                          }
+                          num id = character.id;
+                          context.push('/other-character/$id');
+                        },
+                        child: ClipRRect(
+                          child: Padding(
+                            padding: const EdgeInsets.all(7.0),
+                            child: Stack(
+                              children: [
+                                Positioned.fill(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: ExtendedImage.network(
+                                      mainImageSrc,
+                                      timeLimit:
+                                          ref.watch(imageCacheDurationProvider),
+                                      cacheKey:
+                                          UniqueCacheKeyService.makeUniqueKey(
+                                              mainImageSrc),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                if (!character.is_active) ...[
+                                  ...characterService.addBlur(),
+                                  Center(
+                                    child: Text(
+                                      "공개\n예정",
+                                      style: TextStyle(
+                                        color: ColorConstants.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ).toList());
             }
             return const SizedBox.shrink();
           },
