5622e282b28385afde0aa807707d1b02e38c05e0
ShihyunPark <parksh937@naver.com>
2023-12-13 12:30:57 +0900

    hotfix: 오타 수정, 의견 남기기 중복 제거, 대기 화면 추가

--- lib/screens/all_tab/all_screen.dart
+++ lib/screens/all_tab/all_screen.dart
@@ -182,18 +182,6 @@ class _AllScreenState extends State<AllScreen> {
                 _showWithdrawModal();
               },
             ),
-            QueryBuilder(
-              query: getRefferalCodeQuery(),
-              builder: (context, state) {
-                return MenuWidget(
-                  title: '의견 남기기',
-                  onPressed: () {
-                    launchUrl(Uri.parse(
-                        'https://form.sihyunofjune.com/feedback?ref=${state.data}'));
-                  },
-                );
-              },
-            ),
             const MenuTitleWidget(title: '고객센터'),
             MenuWidget(
               title: '공지',
