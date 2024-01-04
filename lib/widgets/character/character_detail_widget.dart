ac4ac5e5744c8cc14ff548d5ce7de4c8fbeda39b
Arsture <minseok.kim@pygmalion.team>
2024-01-04 12:47:05 +0900

    테스트 완료 화면에만 아래 스크롤 유도 구현

--- lib/widgets/character/character_detail_widget.dart
+++ lib/widgets/character/character_detail_widget.dart
@@ -5,10 +5,11 @@ import 'package:project_june_client/actions/character/models/Character.dart';
 import 'package:project_june_client/actions/character/queries.dart';
 import 'package:project_june_client/providers/character_provider.dart';
 import 'package:project_june_client/widgets/character/profile_widget.dart';
+import 'package:project_june_client/widgets/character/view_others.dart';
 
 import '../../screens/character_test/character_choice_screen.dart';
 
-class CharacterDetailWidget extends ConsumerWidget {
+class CharacterDetailWidget extends ConsumerStatefulWidget {
   final void Function(ActiveScreen) onActiveScreen;
   final void Function(
       {required TestReason reason,
@@ -20,7 +21,23 @@ class CharacterDetailWidget extends ConsumerWidget {
       {super.key, required this.onActiveScreen, required this.onTestInfo});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  CharacterDetailWidgetState createState() => CharacterDetailWidgetState();
+}
+
+class CharacterDetailWidgetState extends ConsumerState<CharacterDetailWidget> {
+  double _toolTipOpacity = 1.0; // 그라데이션과 메시지의 투명도
+
+  void _updateOpacity(ScrollNotification notification) {
+    if (notification.metrics.pixels > 50) {
+      // 스크롤 위치에 따라 조건을 조절하세요
+      setState(() => _toolTipOpacity = 0.0);
+    } else {
+      setState(() => _toolTipOpacity = 1.0);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final query = getPendingTestQuery();
     return QueryBuilder(
       query: query,
@@ -35,47 +52,95 @@ class CharacterDetailWidget extends ConsumerWidget {
           ref.read(characterThemeProvider.notifier).state = character!.theme!;
         });
         return Scaffold(
-          body: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                Expanded(
-                  child: ListView(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 28.0,
-                      vertical: 10,
+          body: Stack(
+            children: [
+              SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 50,
                     ),
-                    children: [
-                      ProfileWidget(
-                        name: character.name!,
-                        characterInfo: character.character_info!,
-                        primaryColor: Color(character.theme!.colors!.primary!),
+                    Expanded(
+                      child: NotificationListener(
+                        onNotification: (ScrollNotification notification) {
+                          _updateOpacity(notification);
+                          return true;
+                        },
+                        child: ListView(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 28.0,
+                            vertical: 10,
+                          ),
+                          children: [
+                            ProfileWidget(
+                              name: character.name!,
+                              characterInfo: character.character_info!,
+                              primaryColor:
+                                  Color(character.theme!.colors!.primary!),
+                            ),
+                            ViewOthersWidget(excludeId: character.id),
+                            FilledButton(
+                              onPressed: () {
+                                widget.onTestInfo(
+                                    reason: testReason == 'NEW_USER'
+                                        ? TestReason.newUser
+                                        : TestReason.retest,
+                                    testId: state.data!['test_id'],
+                                    firstName: character!.first_name!,
+                                    characterId: character.id);
+                                widget.onActiveScreen(ActiveScreen.confirm);
+                              },
+                              child: const Text('다음'),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: IgnorePointer(
+                  ignoring: _toolTipOpacity == 0.0,
+                  child: AnimatedOpacity(
+                    opacity: _toolTipOpacity,
+                    duration: Duration(milliseconds: 500),
+                    child: Container(
+                      padding: EdgeInsets.all(32),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Colors.black.withOpacity(0.8),
+                            Colors.transparent
+                          ],
+                        ),
+                      ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: Colors.white,
+                              size: 32,
+                            ),
+                            Text(
+                              '다른 상대도 살펴보세요!',
+                              style: TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 28.0, right: 28.0, bottom: 20.0),
-                  child: FilledButton(
-                    onPressed: () {
-                      onTestInfo(
-                          reason: testReason == 'NEW_USER'
-                              ? TestReason.newUser
-                              : TestReason.retest,
-                          testId: state.data!['test_id'],
-                          firstName: character!.first_name!,
-                          characterId: character.id);
-                      onActiveScreen(ActiveScreen.confirm);
-                    },
-                    child: const Text('다음'),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
